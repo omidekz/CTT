284b627825e68dd9df778c6928aeb8dfae180720
omidekz <omidekz@gmail.com>
2020-10-05 15:56:06 +0330

    imprv

--- filer.py
+++ filer.py
@@ -22,12 +22,12 @@ class Manager(abstracts.BaseManager):
         self.data = pickle.load(file)
         file.close()
     
-    def write(self, lable, time):
+    def write(self, lable, time=None):
         if not time:
             time = dt.now()
         if self.data.get(lable, None):
             data = self.data.get(lable)[-1]
-            self.data.append({
+            self.data[lable].append({
                 "time": time,
                 "state": abstracts
                             .STATES
@@ -42,13 +42,43 @@ class Manager(abstracts.BaseManager):
                     "state": abstracts.STATES.PROGRESS
                 }
             ]
+        for loop_lable, data in self.data.items():
+            if lable == loop_lable:
+                continue
+            if abstracts \
+                .STATES \
+                .is_progress(data[-1]['state']):
+                data.append({
+                    "time": time,
+                    "state": abstracts.STATES.END
+                })
+            self.data[loop_lable] = data
         self._update_db()
     
     def read(self, lable, raise_error=False):
         self._update_data()
         if self.exists(lable):
             data = self.data[lable]
-            return [-1, lable, abstracts.STATES.str(data[-1]['state'])]
+            whole = 0
+            tmp_a = 0
+            tmp_b = 0
+            last_progress_calc = False
+            for i in range(len(data)):
+                if i % 2 == 0:
+                    # state is PROGRESS
+                    tmp_a = data[i]['time'].timestamp()
+                else:
+                    # state is END
+                    tmp_b = data[i]['time'].timestamp()
+                    whole += (tmp_b - tmp_a)
+ 
+            if len(data) % 2 == 1:
+                whole += (
+                    dt.now().timestamp() 
+                    - data[-1]['time'].timestamp()
+                )
+
+            return [int(whole), lable, abstracts.STATES.str(data[-1]['state'])]
         if raise_error:
             raise Exception("No Lable Exist")
     
