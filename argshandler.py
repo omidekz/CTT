e7f5e39a4ca878235aa45ac97f91d2d06b84f7cd
omidekz <omidekz@gmail.com>
2020-10-10 23:26:15 +0330

    make my custom handler

--- argshandler.py
+++ argshandler.py
@@ -1,3 +1,23 @@
+from bases.argshandler import Handler
 
-class Handler:
-    pass
+class CustomHandler(Handler):
+    def __init__(self, args):
+        super().__init__(args)
+    
+    def all(self):
+        return self.args.all
+    
+    def db(self):
+        return self.args.db
+    
+    def toggle(self):
+        return self.args.toggle
+
+    def new(self):
+        return self.args.new
+    
+    def update(self):
+        return self.args.update
+    
+    def status(self):
+        return self.args.status
