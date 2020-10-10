abb2f11956ad9e0a2abff5611ab37e648de460b6
omidekz <omidekz@gmail.com>
2020-10-10 23:57:52 +0330

    change arger

--- arger.py
+++ arger.py
@@ -2,39 +2,40 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-db_or_toggle = parser.add_mutually_exclusive_group(
+group = parser.add_mutually_exclusive_group(
         
 )
-db_or_toggle.add_argument(
+
+group.add_argument(
     '-d', '--db', type=str,
     nargs='?',
     help='the name of db, default is last db that used'
 )
-db_or_toggle.add_argument(
+
+group.add_argument(
     '-t', '--toggle', type=str,
     help='toggle a task'
 )
-db_or_toggle.add_argument(
+
+group.add_argument(
     '-n', '--new', nargs='+', type=str,
     help='new lable(s)'
 )
-db_or_toggle.add_argument(
+
+group.add_argument(
     '-u', '--update', help='update a lable. lable present the name of task\nthat must pass in first',
     nargs='+'
 )
 
-notice_commands = parser.add_mutually_exclusive_group(
-
+group.add_argument(
+    '-a', '--all', action='store_true',
+    help='status of all tasks[-a] or DBs[-ad]'
 )
 
-notice_commands.add_argument(
+group.add_argument(
     '-s', '--status',
     type=str,
     help='status of task'
-)
-notice_commands.add_argument(
-    '-a', '--all', action='store_true',
-    help='status of all tasks[-a] or DBs[-ad]'
 )
 
 parser.add_argument(
